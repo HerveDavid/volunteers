21a4d98a7aacf875f1e5857270254ad5f0eaf108
Shaïnee <shaynetwok@gmail.com>
2021-11-02 14:49:25 +0100

    [SHAINEE] ajout lecture et suppression duplicat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -23,10 +25,56 @@ public class App {
             }
         }
 
-        // Apply dark magic here...
         lines.forEach(System.out::println);
+        System.out.println("Nombre d'itération : " + lines.stream().count());
+/*
+
+        String crunchifyCSVFile = "src/main/resources/data.csv";
+
+        BufferedReader crunchifyBufferReader = null;
+        String crunchifyLine = "";
+        int i = 0;
+        HashSet<String> crunchifyAllLines = new HashSet<>();
+
+        try {
+            crunchifyBufferReader = new BufferedReader(new FileReader(crunchifyCSVFile));
+            while ((crunchifyLine = crunchifyBufferReader.readLine()) != null) {
+                if (crunchifyAllLines.add(crunchifyLine)) {
+                    crunchifyLog(crunchifyLine);
+                    i++;
+                } else if (!crunchifyIsNullOrEmpty(crunchifyLine)) {
+                    crunchifyLog("--------------- Ligne sautee : " + crunchifyLine);
+                }
+            }
+            crunchifyLog("Nombre d'itération : " + i );
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (crunchifyBufferReader != null) {
+                try {
+                    crunchifyBufferReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }*/
 
     }
+/*
+
+    public static boolean crunchifyIsNullOrEmpty(String crunchifyString) {
+
+        if (crunchifyString != null && !crunchifyString.trim().isEmpty())
+            return false;
+        return true;
+    }
+
+    // Simple method for system outs
+    private static void crunchifyLog(String s) {
+
+        System.out.println(s);
+    }
+*/
 
     public static Function<String, String[]> splitCSV = (row) -> row.split(";", -1);
 
